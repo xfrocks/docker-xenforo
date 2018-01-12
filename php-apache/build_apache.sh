fb60b7f62674fa79c6b4b547e3254409e2ace6e9
Dao Hoang Son <daohoangson@gmail.com>
2018-01-12 13:34:36 +0700

    [php-apache] Enable default-ssl site

--- php-apache/build_apache.sh
+++ php-apache/build_apache.sh
@@ -2,8 +2,20 @@
 
 set -e
 
+# phpcs
 pear install PHP_CodeSniffer
-
 curl -Lo /tmp/PHPCompatibility.tar.gz "https://github.com/wimg/PHPCompatibility/archive/${PHP_COMPATIBILITY_VERSION}.tar.gz"
 tar -xzf /tmp/PHPCompatibility.tar.gz -C /usr/local/share/
 phpcs --config-set installed_paths "/usr/local/share/PHPCompatibility-${PHP_COMPATIBILITY_VERSION}"
+
+# tls
+openssl req \
+  -days 365 \
+  -keyout /etc/ssl/private/ssl-cert-snakeoil.key \
+  -newkey rsa:4096 \
+  -nodes \
+  -out /etc/ssl/certs/ssl-cert-snakeoil.pem \
+  -subj "/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd/CN=php-apache.docker" \
+  -x509
+a2enmod ssl
+a2ensite default-ssl

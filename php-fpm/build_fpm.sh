036c557bfe22fd52879ed901d6345da70f02dba1
Dao Hoang Son <daohoangson@gmail.com>
2019-03-09 18:33:19 +0700

    Remove Tideways from php-fpm image (incompatible with php 7.3)

--- php-fpm/build_fpm.sh
+++ php-fpm/build_fpm.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-# tideways
-curl -Lo /tmp/tideways.tar.gz "https://s3-eu-west-1.amazonaws.com/tideways/extension/${TIDEWAYS_VERSION}/tideways-php-${TIDEWAYS_VERSION}-x86_64.tar.gz"
-tar -xzf /tmp/tideways.tar.gz -C /tmp/
-_installPath="/tmp/tideways-php-${TIDEWAYS_VERSION}/install.sh"
-chmod +x "${_installPath}" && "${_installPath}"
-docker-php-ext-enable tideways
-{ \
-  echo 'tideways.auto_start=0'; \
-  echo 'tideways.auto_prepend_library=0'; \
-} > /usr/local/etc/php/conf.d/zzz-tideways.ini
-
 # xdebug: installed but disabled on php-fpm
 sed -i'' 's/^/;/' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
